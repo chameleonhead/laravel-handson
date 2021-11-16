9673bd2923324ea0fa51d6cc87f31396e3b8936d
Yuto Nagano <chamelion@hotmail.co.jp>
2021-11-16 10:14:27 +0000

    softdelete likes

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Like extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'user_id',
         'post_id'

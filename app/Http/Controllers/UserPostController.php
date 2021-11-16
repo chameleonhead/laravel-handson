4eb447262fed1f71438c23d53d4c76f9f030c0d1
Yuto Nagano <chamelion@hotmail.co.jp>
2021-11-16 08:40:57 +0000

    create post view component

--- app/Http/Controllers/UserPostController.php
+++ app/Http/Controllers/UserPostController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class UserPostController extends Controller
 {
     public function index(User $user)
     {
-        return view('users.posts');
+        $posts = $user->posts()->with(['user', 'likes'])->paginate(20);
+        return view('users.posts.index', [
+            'user' => $user,
+            'posts' => $posts
+        ]);
     }
 }

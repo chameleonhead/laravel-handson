399b6c6b32032540fa8d00200f7d9776bf27fade
Yuto Nagano <chamelion@hotmail.co.jp>
2021-11-16 06:08:31 +0000

    create posts index page

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -9,4 +10,9 @@ class Post extends Model
     protected $fillable = [
         'body'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
